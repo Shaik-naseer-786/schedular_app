afd818cffca31946c3bea7db34fc9e38ff0e02d2
Naseer <sknaseer9347@gmail.com>
2025-09-14 13:43:31 +0530

    new file added

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,12 +2,13 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import { MongoClient } from "mongodb";
+import type { Adapter } from "next-auth/adapters";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const clientPromise = client.connect();
 
 export const authOptions: NextAuthOptions = {
-  adapter: MongoDBAdapter(clientPromise) as any,
+  adapter: MongoDBAdapter(clientPromise) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
